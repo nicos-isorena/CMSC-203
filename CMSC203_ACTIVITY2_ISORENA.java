edffd733148704a6bf11b588f5cedca78b4ef808
nicos-isorena <112452492+nicos-isorena@users.noreply.github.com>
2022-09-09 09:01:29 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY2_ISORENA.java
@@ -0,0 +1,22 @@
+public class CMSC203_ACTIVITY2_ISORENA {
+    
+    public static void main(String[] args)
+    {
+        String Firstname = "Nicos";
+        String Lastname= "Isorena";
+        int age = 19;
+        float gpa = 1.50f;
+        char bloodtype = 'O';
+
+
+        System.out.println("Hi, My name is " + Firstname + " " + Lastname);
+
+        System.out.println("I am " + age + "years old");
+
+        System.out.println("My GPA is " + gpa);
+
+        System.out.println("My Bloodtype is " + bloodtype);
+
+
+    }
+}
