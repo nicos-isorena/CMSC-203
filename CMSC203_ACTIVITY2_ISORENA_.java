13ce2bf3fc8d5fb0cb46403c5b7bce808ada3770
nicos-isorena <112452492+nicos-isorena@users.noreply.github.com>
2022-09-14 08:20:19 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY2_ISORENA_.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CMSC203_ACTIVITY2_ISORENA_ {
+    public static void main(String[] args)
+    {
+        float num1;
+
+        float num2;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        
+        System.out.println("FINDING THE PRODUCT OF NUM 1 and NUM 2 ");
+
+
+        System.out.print("PLEASE INPUT ANY VALUE FOR NUM 1 = ");
+
+        num1 = scan.nextFloat();
+
+        System.out.println("YOU PRESSED NUMBER " + num1);
+
+        System.out.print("PLEASE INPUT ANY VALUE FOR NUM 2 = ");
+        
+        num2 = scan.nextFloat();
+
+        System.out.println("YOU PRESSED NUMBER " + num2);
+
+
+        System.out.println("THE PRODUCT OF NUM 1 + NUM 2 is = " + num1 * num2);
+
+
+
+
+
+
+
+
+
+
+
+     }
+}
